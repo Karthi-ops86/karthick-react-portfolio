b85fc69992ea9dbd5349a44a5448279d41db76e6
karthick M <karthick.m0848@gmail.com>
2026-03-21 21:03:09 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -92,9 +92,9 @@ export const Hero = () => {
 
             {/* CTAs */}
             <div className="flex flex-wrap gap-4 animate-fade-in animation-delay-300">
-              <Button size="lg">
+              {/* <Button size="lg">
                 Contact Me <ArrowRight className="w-5 h-5" />
-              </Button> 
+              </Button>  */}
               <a href="/public/Download/Karthick_M_Resume (1).pdf" download>
               <AnimatedBorderButton>
                 <Download  className="w-5 h-5" />
